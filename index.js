683fae1f6e82e8b456f14ef86d93a0e1a3f32c13
Rafaella Silveira <rafaellagelsilveira@gmail.com>
2023-11-10 10:12:36 -0300

    node e express finalizado

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+//https://localhost3000/usuarios
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuario: ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
+//https://localhost3000
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
